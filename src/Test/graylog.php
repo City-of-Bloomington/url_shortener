7ba9e44cf900c4b31fd266715ebde1f5cbbd1d51
Cliff Ingham <inghamn@bloomington.in.gov>
2024-10-07 14:02:47 -0400

    Graylog: added script for testing graylog integration
    Updates #28

---
+++ src/Test/graylog.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @copyright 2024 City of Bloomington, Indiana
+ * @license https://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
+ */
+declare (strict_types=1);
+include '../Web/bootstrap.php';
+
+$d = new FakeClass();
